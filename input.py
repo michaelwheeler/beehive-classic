002baf80fefdf5d8c0fc7296be4e2afe80b7993b
Michael Wheeler <mpwheel@umich.edu>
2023-09-03 19:02:55 -0400

    Allow starting game with controller

--- input.py
+++ input.py
@@ -6,7 +6,15 @@ def quit():
 
 
 def start():
-    return pyxel.btnp(pyxel.KEY_SPACE)
+    return any(
+        (
+            pyxel.btnp(pyxel.KEY_SPACE),
+            pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A),
+            pyxel.btnp(pyxel.GAMEPAD1_BUTTON_B),
+            pyxel.btnp(pyxel.GAMEPAD1_BUTTON_X),
+            pyxel.btnp(pyxel.GAMEPAD1_BUTTON_Y),
+        )
+    )
 
 
 def launch():
